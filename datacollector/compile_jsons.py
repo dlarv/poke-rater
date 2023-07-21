b554e95eda11036d7f237926387b30d8c7fe3180
dlarv <hyrum21344@gmail.com>
2023-07-20 21:43:00 -0400

    Gathered data that datacollector missed

--- datacollector/compile_jsons.py
+++ datacollector/compile_jsons.py
@@ -13,6 +13,7 @@ PATH = f"{os.path.dirname(__file__)}/data"
 # - Sneasler
 # - Charizard: Redotherformsmayhaveothercolors.
 #
+# Farfetchd pic didn't download
 # Some manga/anime appearances are null, must be 0
 
 def single_file():
@@ -26,6 +27,35 @@ def single_file():
                 output[obj_name] = obj
         stream.write(json.dumps(output))
 
+def clean_data(pokemon: dict):
+    '''Fix mistakes inside json files'''
+    # Missing Id num
+    if not pokemon.get("name") or not pokemon.get("dex_no"):
+        raise KeyError("Error in json. Could not parse pokemon")
+
+    # Values that cannot be null & unsalvagable
+    for key in ("gen_no", "color", "typing", "matchups", "stats"):
+        if not pokemon.get(key):
+            raise KeyError(f"{pokemon['name']} ({pokemon['dex_no']}) corrupted. Could not parse")
+
+    # Values that cannot be null
+    for key in ("manga_count", "anime_count", "stat_total"):
+        if pokemon.get(key) is None:
+            print(f"WARNING: {pokemon['name']} ({pokemon['dex_no']}) missing {key}, setting value to 0.")
+            pokemon[key] = 0
+    
+    # Lists that cannot have null members
+    for key in ("matchups", "stats"):
+        if not isinstance(pokemon.get(key), (list, tuple, dict)):
+            print(f"WARNING: {pokemon['name']} ({pokemon['dex_no']}) has {key}, but it is not an iterable.")
+            continue
+        for item in pokemon.get(key):
+            if item is None:
+                print(f"WARNING: {pokemon['name']} ({pokemon['dex_no']}): {item} in {key} is None. Setting to 0")
+                pokemon[key][item] = 0
+
+    return pokemon
+
 def compile_slides():
     '''Compile jsons with slides'''
     output = []
@@ -38,6 +68,7 @@ def compile_slides():
         # Pokemon was already read (e.g. pikachu=25, pichu=172)
         if i not in numbers:
             continue
+
         file_path = f"{PATH}/{i}.json"
         # numbers.remove(i)
         with open(file_path, 'r', encoding='utf-8') as stream:
@@ -50,29 +81,33 @@ def compile_slides():
             slide_num += 1
             log_output = ""
 
-            # Pokemon does not evolve
-            if related is None:
-                log_output = f"({i:04d}) {pokemon['name']:<12}"
-                print(log_output)
-                group.append(pokemon)
-                continue
+            try:
+                # Pokemon does not evolve
+                if related is None:
+                    group.append(clean_data(pokemon))
+                    log_output = f"({i:04d}) {pokemon['name']:<12}"
+                    print(log_output)
+                    continue
 
-            # Get evos
-            for other in related:
-                # evo was already read
-                if other not in numbers:
-                    continue
-                if other == i:
-                    numbers.remove(i)
-                    group.append(pokemon)
-                    log_output += f" ({i:04d}) {pokemon['name']:<12}"
-                    continue
-                numbers.remove(other)
-                with open(f"{PATH}/{other}.json", 'r', encoding='utf-8') as other_stream:
-                    # Write evo
-                    other_pokemon = json.load(other_stream)
-                    group.append(other_pokemon)
-                    log_output += f" ({other:04d}) {other_pokemon['name']:<12}"
+                # Get evos
+                for other in related:
+                    # evo was already read
+                    if other not in numbers:
+                        continue
+
+                    if other == i:
+                        numbers.remove(i)
+                        group.append(clean_data(pokemon))
+                        log_output += f" ({i:04d}) {pokemon['name']:<12}"
+                        continue
+                    numbers.remove(other)
+                    with open(f"{PATH}/{other}.json", 'r', encoding='utf-8') as other_stream:
+                        # Write evo
+                        other_pokemon = json.load(other_stream)
+                        group.append(clean_data(other_pokemon))
+                        log_output += f" ({other:04d}) {other_pokemon['name']:<12}"
+            except KeyError as err:
+                print(err)
 
             print(log_output)
     output_file.write(json.dumps(output))
